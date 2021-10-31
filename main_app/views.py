aa4522bb36900978a58b3f6d7673ba7bcbeffed3
CB-Pantomime <christopherbrown.contact@gmail.com>
2021-10-31 16:28:46 -0700

    added index path and index class, added profile show page path and profile class, and profile_detail.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
+
+
 from django.shortcuts import render
+from django.views import View
+
+from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
+from main_app.models import Profile
 
 # Create your views here.
+class Index(TemplateView):
+
+    template_name = "index.html"
+
+
+class ProfileDetail(DetailView):
+
+    model = Profile
+    template_name = "profile_detail.html"
+
+
