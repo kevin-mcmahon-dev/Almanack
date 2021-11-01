8f809cab3e6d4e1dbbee50c0cbcd0efa8494c023
kevinmcm1229 <kevinmcm1229@gmail.com>
2021-10-31 23:15:54 -0600

    Added user sign up and sign in. Verified user creation through /admin

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
@@ -7,6 +6,7 @@ urlpatterns = [
 
     path('', views.Index.as_view(), name ="index"),
     path('profile/<int:pk>/', views.ProfileDetail.as_view(), name ="profile_detail"),
+    path('accounts/signup/', views.Signup.as_view(), name="signup"),
 
     # path('profile/<int:pk>/posts/<int:pk>')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,16 @@
 
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-
 from django.views.generic.base import TemplateView
 from django.views.generic import DetailView
 from main_app.models import Profile
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 
@@ -13,11 +18,25 @@ class Index(TemplateView):
 
     template_name = "index.html"
 
-
 class ProfileDetail(DetailView):
 
     model = Profile
     template_name = "profile_detail.html"
 
-
+class Signup(View):
+    # show a form to fill out
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+    # on form ssubmit validate the form and login the user.
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("index")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
 
