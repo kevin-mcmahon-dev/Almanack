bf01cd717c20d55828cd01fd1961d6ad581d32f6
marcos323s <saucedomarcos1@yahoo.com>
2021-11-03 09:27:39 -0700

    added alot of styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
 
     path('', views.Index.as_view(), name ="index"),
     path('profile/<int:pk>/', views.ProfileDetail.as_view(), name ="profile_detail"),
-
+    path('post-show', views.PostShow.as_view(), name='post-show'),
+    path('cities', views.Cities.as_view(), name='cities'),
+    path('user-public', views.UserPublic.as_view(), name='user')
     # path('profile/<int:pk>/posts/<int:pk>')
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -5,19 +5,33 @@ from django.views import View
 
 from django.views.generic.base import TemplateView
 from django.views.generic import DetailView
-from main_app.models import Profile
+
+from main_app.models import Profile, City
 
 # Create your views here.
 
 class Index(TemplateView):
-
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cities'] = City.objects.all()
+        return context
 
+        
 class ProfileDetail(DetailView):
-
-    model = Profile
+    
     template_name = "profile_detail.html"
 
 
+class PostShow(TemplateView):
+    
+    template_name = 'post-show.html'
 
+
+class Cities(TemplateView):
+    template_name = 'cities.html'
+ 
+
+class UserPublic(TemplateView):
+    template_name = 'user-public.html'
